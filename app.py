617646c424ebbe09ab54e5c0d8c7ab1cb347c264
Awani Caleb <calebawani@gmail.com>
2025-12-14 22:05:46 +0100

    FEAT: Add about page route

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-    return 'Hello George Simisola, I\'m Caleb hehe!'
+    return 'Hello Simisola, I\'m Caleb hehe!'
 
+@app.route('/about')
+
+def about_page():
+    return 'This is the official About page for my cool Python backend'
