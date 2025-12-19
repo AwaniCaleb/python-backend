0ba32d138ad189c381b35198fbdd2da37febb8ae
Awani Caleb <calebawani@gmail.com>
2025-12-19 18:03:26 +0100

    FEAT: Add User model and configure SQLAlchemy for database integration

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
+import os
+
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(120), nullable=False) 
+
+    def __repr__(self):
+        return f'<User {self.email}>'
 
 @app.route('/')
 
